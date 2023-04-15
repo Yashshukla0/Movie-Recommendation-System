743a20016f0b5f1e54d3c5d6a45e9c0a7293bb9e
Yash Shukla <95163045+Yashshukla0@users.noreply.github.com>
2023-04-16 01:06:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,10 +10,13 @@ def recommend(movie):
     for i in movie_list:
         recommended.append(movies.iloc[i[0]].title)
     return recommended
-with open('similarity.pkl') as similarity:
 
+with open('similarity.pkl') as sim:
+    similarity=pickle.load(sim)
+with open("movie_dict.pkl") as tim:
+    movies_dict=pickle.load(tim)
 # similarity=pickle.load(open('similarity.pkl','rb'))
-movies_dict=pickle.load(open('movie_dict.pkl','rb'))
+# movies_dict=pickle.load(open('movie_dict.pkl','rb'))
 movies=pd.DataFrame(movies_dict)
 
 st.title("Movie Recommendation System")
